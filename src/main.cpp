b83235590dd2f9a720ffc8aa436e6a81985a4782
Balazs Kelemen <10376327+prampec@users.noreply.github.com>
2020-02-25 23:52:08 +0100

    Moved to ArduinoUNO + stability enhancements.

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,47 @@
 /**
- * https://stm32-base.org/boards/STM32F103C8T6-Blue-Pill
- * PinOut: https://solovjov.net/reblag.dk/The-Generic-STM32F103-Pinout-Diagram.jpg
- * PinOut2: https://opencircuit.shop/resources/content/ad8542259ac19/crop/900-600/STM32-ARM-development-board-STM32F103C8T6.jpg
- * Pins:
+ * Arduino UNO
  */
 
 #include <Arduino.h>
 #include <SoftTimer.h>
 #include <BlinkTask.h>
 
-#define PIN_LED PC13
+#define PIN_LED 13
 
-#define PIN_ULTRASONIC_TRIG PB11
-#define PIN_ULTRASONIC_ECHO PB10 // Must be 5V tolerant pin!
-//#define ULTRASONIC_TIMEOUT_MICROS 2500
-#define ULTRASONIC_TIMEOUT_MICROS 4000
+#define PIN_ULTRASONIC_TRIG 7
+#define PIN_ULTRASONIC_ECHO 2
+#define ULTRASONIC_TIMEOUT_MICROS 2500
 
-#define PIN_L2 PA0
-#define PIN_L1 PA2
-#define PIN_MD PA1
-#define PIN_R1 PA3
-#define PIN_R2 PA4
+#define PIN_L2 12
+#define PIN_L1 11
+#define PIN_MD 9
+#define PIN_R1 10
+#define PIN_R2 8
 
-#define PIN_MOTOR_LEFT_FWD PB7
-#define PIN_MOTOR_LEFT_BWD PB6
-#define PIN_MOTOR_RIGHT_FWD PB8
-#define PIN_MOTOR_RIGHT_BWD PB9
+#define PIN_MOTOR_LEFT_FWD  3
+#define PIN_MOTOR_LEFT_BWD  4 // Non PWM pin!
+#define PIN_MOTOR_RIGHT_FWD 5
+#define PIN_MOTOR_RIGHT_BWD 6
 
 #define MOTOR_MIN_SPEED 30
-#define MOTOR_SPEED_SLOW 90
-#define MOTOR_SPEED_FAST 255
+#define MOTOR_SPEED_SLOW 70
+#define MOTOR_SPEED_FAST 130
 
-#define PIN_BUTTON PB12
+#define PIN_BUTTON A0
 
 // States
 #define LIFECYCLE_STATE_READY 0
 #define LIFECYCLE_STATE_RUNNING 1
 
 int32_t getUltrasonicDistance();
-void echoChanged(void);
 void sendTrigger();
 void goRight(int val);
 void goLeft(int val);
 void doAlways(Task* me);
 void debounce();
 void onButtonPressed(uint8_t pinNr);
+void onButtonReleased(uint8_t pinNr);
+void stop();
 
 int buttonState = HIGH;             // the current reading from the input pin
 int lastReading = HIGH;   // the previous reading from the input pin
@@ -60,6 +57,7 @@ bool rightFromTrack;
 bool leftFromTrack;
 bool objectInFront = false;
 volatile unsigned long echoChangedTime;
+unsigned long lastOnTrack = 0;
 
 byte liefcycleState = LIFECYCLE_STATE_READY;
 
@@ -91,17 +89,15 @@ void setup()
   pinMode(PIN_MOTOR_RIGHT_FWD, OUTPUT);
   pinMode(PIN_MOTOR_RIGHT_BWD, OUTPUT);
 
-  analogWriteFrequency(500);
-
   hartbeat.onLevel = LOW;
   hartbeat.start();
-  attachInterrupt(PIN_ULTRASONIC_ECHO, echoChanged, CHANGE);
   SoftTimer.add(&alwaysTask);
   Serial.println("Ready.");
 }
 
 void doAlways(Task* me)
 {
+  unsigned long now = millis();
   int32_t distanceFromObject = getUltrasonicDistance();
 //  Serial.println(distanceFromObject);
 
@@ -137,6 +133,11 @@ void doAlways(Task* me)
   bool trackOnRight = digitalRead(PIN_R1);
   if (onTrack)
   {
+    lastOnTrack = now;
+  }
+
+  if ((now - lastOnTrack) < 3000)
+  {
     if (trackOnLeft)
     {
       leftFromTrack = true;
@@ -151,6 +152,12 @@ void doAlways(Task* me)
       leftFromTrack = false;
     }
   }
+  else
+  {
+    stop();
+    return;
+  }
+
   if (rightFromTrack)
   {
     // Right side has priority.
@@ -222,72 +229,13 @@ void goLeft(int val)
   }
 }
 
-void testOpto()
-{
-  unsigned long start = micros();
-  uint32_t a0 = digitalRead(PA0);
-  uint32_t a1 = digitalRead(PA1);
-  uint32_t a2 = digitalRead(PA2);
-  uint32_t a3 = digitalRead(PA3);
-  uint32_t a4 = digitalRead(PA4);
-  unsigned long end = micros();
-  Serial.print(end - start);
-  Serial.print(" ");
-  Serial.print(a0);
-  Serial.print(" ");
-  Serial.print(a1);
-  Serial.print(" ");
-  Serial.print(a2);
-  Serial.print(" ");
-  Serial.print(a3);
-  Serial.print(" ");
-  Serial.print(a4);
-}
-
 int32_t getUltrasonicDistance()
 {
-//  attachInterrupt(PIN_ULTRASONIC_ECHO, echoChanged, CHANGE);
   sendTrigger();
 
-  unsigned long startTime = micros();
-  unsigned long prevEchoChangedTime = echoChangedTime;
-
-  // TODO: might want to asynchronous working instead of using wait loops.
-  while(1)
-  {
-    if (prevEchoChangedTime != echoChangedTime)
-    {
-      break;
-    }
-    if ((micros()-startTime) > ULTRASONIC_TIMEOUT_MICROS)
-    {
-      return -1;
-    }
-  }
-
-  unsigned long frameStartBegins = prevEchoChangedTime = echoChangedTime;
-
-  while(1)
-  {
-    if (prevEchoChangedTime != echoChangedTime)
-    {
-      break;
-    }
-    if ((micros()-startTime) > ULTRASONIC_TIMEOUT_MICROS)
-    {
-      return -1;
-    }
-  }
-
-//  detachInterrupt(PIN_ULTRASONIC_ECHO);
-
-  long time = echoChangedTime - frameStartBegins;
-
-  return time / 59;
-}
-void echoChanged(void)
-{
-  echoChangedTime = micros();
+  pinMode(PIN_ULTRASONIC_ECHO, INPUT);
+  unsigned long duration = pulseIn(PIN_ULTRASONIC_ECHO, HIGH, ULTRASONIC_TIMEOUT_MICROS);
+  return duration / 58;
 }
 
 void sendTrigger()
@@ -302,24 +250,29 @@ void sendTrigger()
 // -- When Start/Stop button pressed
 void onButtonPressed(uint8_t pinNr)
 {
-	if (liefcycleState == LIFECYCLE_STATE_RUNNING)
-  {
-    goRight(0);
-    goLeft(0);
-    Serial.println(F("Stop"));
-    liefcycleState = LIFECYCLE_STATE_READY;
-    hartbeat.onMs = 700;
-    hartbeat.offMs = 700;
-  }
 }
 // -- When Start/Stop button released
 void onButtonReleased(uint8_t pinNr)
 {
-	if (liefcycleState == LIFECYCLE_STATE_READY)
+	if (liefcycleState == LIFECYCLE_STATE_RUNNING)
+  {
+    stop();
+  }
+	else if (liefcycleState == LIFECYCLE_STATE_READY)
   {
     Serial.println(F("Start"));
     liefcycleState = LIFECYCLE_STATE_RUNNING;
     hartbeat.onMs = 100;
     hartbeat.offMs = 100;
   }
+}
+
+void stop()
+{
+    goRight(0);
+    goLeft(0);
+    Serial.println(F("Stop"));
+    liefcycleState = LIFECYCLE_STATE_READY;
+    hartbeat.onMs = 700;
+    hartbeat.offMs = 700;
 }
